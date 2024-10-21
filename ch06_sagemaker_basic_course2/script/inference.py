e802ec604966a51ae03a34216d489b995859cd50
dante <datapod.k@gmail.com>
2024-10-21 16:10:55 +0900

    ucimlrepo 패키지 제거 및 스크립트 수정

--- ch06_sagemaker_basic_course2/script/inference.py
+++ ch06_sagemaker_basic_course2/script/inference.py
@@ -9,16 +9,23 @@ import boto3
 
 def model_fn(model_dir):
     """XGBoost 모델과 필요한 자산을 `model_dir`에서 로드합니다."""
+    # 모델 객체 로드
     model_file = 'xgboost-model'
-    booster = pkl.load(open(os.path.join(model_dir, model_file), 'rb'))
+    xgb_model = pkl.load(open(os.path.join(model_dir, model_file), 'rb'))
     
     # S3에서 asset 파일을 로컬로 복사
     s3 = boto3.client('s3')
     bucket_name = 'dante-sagemaker'
-    project_name = 'income-prediction'
-    scaler_key = f'{project_name}/asset/scaler.pkl'
-    encoder_key = f'{project_name}/asset/encoder.pkl'
-    pca_key = f'{project_name}/asset/pca.pkl'
+    project_name = 'adult-income-classification-v2'
+    
+    # 자산 파일 로드
+    scaler_key = f'{project_name}/asset/adult_scaler.pkl'
+    encoder_key = f'{project_name}/asset/adult_encoders.pkl'
+    pca_key = f'{project_name}/asset/adult_pca.pkl'
+    
+    local_scaler_path = os.path.join(model_dir, 'adult_scaler.pkl')
+    local_encoder_path = os.path.join(model_dir, 'adult_encoders.pkl')
+    local_pca_path = os.path.join(model_dir, 'adult_pca.pkl')
     
     scaler_obj = s3.get_object(Bucket=bucket_name, Key=scaler_key)
     encoder_obj = s3.get_object(Bucket=bucket_name, Key=encoder_key)
@@ -28,13 +35,7 @@ def model_fn(model_dir):
     encoders = pkl.loads(encoder_obj['Body'].read())
     pca = pkl.loads(pca_obj['Body'].read())
     
-    original_feature_columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
-       'marital-status', 'occupation', 'relationship', 'race', 'sex',
-       'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
-    
-    numeric_columns = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
-    
-    return booster, (scaler, encoders, pca, original_feature_columns, numeric_columns)
+    return xgb_model, (scaler, encoders, pca)
 
 def input_fn(request_body, request_content_type):
     """입력 데이터 페이로드를 파싱합니다."""
@@ -43,34 +44,39 @@ def input_fn(request_body, request_content_type):
     df = pd.read_csv(io.StringIO(request_body), header=None)
     return df.values
 
-def predict_fn(input_data, model):
-    """로드된 모델로 예측을 수행합니다."""
-    booster, (scaler, encoders, pca, original_feature_columns, numeric_columns) = model
-    prep_input_data = preprocess_input_data(input_data, (scaler, encoders, pca, original_feature_columns, numeric_columns))
-    dmatrix = xgb.DMatrix(prep_input_data)
-    return booster.predict(dmatrix)
-
 def output_fn(prediction, accept):
     """예측 출력을 포맷팅합니다."""
     if accept != "text/csv":
         raise ValueError(f"지원되지 않는 accept 타입입니다: {accept}")
     return ','.join(map(str, prediction))
 
+def predict_fn(input_data, model):
+    """로드된 모델로 예측을 수행합니다."""
+    xgb_model, (scaler, encoders, pca) = model
+    prep_input_data = preprocess_input_data(input_data, (scaler, encoders, pca))
+    dmatrix = xgb.DMatrix(prep_input_data)
+    return xgb_model.predict(dmatrix)
+
+
 def preprocess_input_data(input_data, assets):
     """입력 데이터를 전처리합니다."""
-    scaler, encoders, pca, original_feature_columns, numeric_columns = assets
-    X = pd.DataFrame(input_data, columns=original_feature_columns)
+    scaler, encoders, pca = assets
+    
+    total_cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
+       'marital-status', 'occupation', 'relationship', 'race', 'sex',
+       'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
+    numeric_cols = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss',
+       'hours-per-week']
+
+    
+    X = pd.DataFrame(input_data, columns=total_cols)
+
+    # 전처리
     X[X == '?'] = np.nan
     X['workclass'].fillna(X['workclass'].mode()[0], inplace=True)
     X['occupation'].fillna(X['occupation'].mode()[0], inplace=True)
     X['native-country'].fillna(X['native-country'].mode()[0], inplace=True)
-    
-    # 숫자형 컬럼을 float64로 변환하기 전에 NaN 값을 처리합니다
-    for col in numeric_columns:
-        X[col] = pd.to_numeric(X[col], errors='coerce')
-    
-    X[numeric_columns] = X[numeric_columns].astype('float64')
-    X[numeric_columns] = scaler.transform(X[numeric_columns])
+    X[numeric_cols] = X[numeric_cols].astype('float64')
     
     # 범주형 컬럼 레이블 인코딩
     for feature in encoders.keys() :
@@ -81,11 +87,12 @@ def preprocess_input_data(input_data, assets):
         le.classes_ = np.unique(np.concatenate([le.classes_, unique_values]))
         # 변환 처리
         X[feature] = le.transform(X[feature])
-        
-    # NaN 및 무한대 값 처리
-    X = X.replace([np.inf, -np.inf], np.nan)
-    X = X.fillna(X.mean())
+
+    # 스케일링
+    X_scaled = scaler.transform(X)
+
+    # PCA 차원축소
+    X_pca = pca.transform(X_scaled)
     
-    # PCA 변환 수행
-    X_pca = pd.DataFrame(pca.transform(X), columns=[f'PC{i}' for i in range(1, pca.n_components_ + 1)])
-    return X_pca
+    return pd.DataFrame(X_pca, columns=[f'PC{i}' for i in range(1, pca.n_components_ + 1)])
+   
--- ch06_sagemaker_basic_course2/script/train.py
+++ ch06_sagemaker_basic_course2/script/train.py
@@ -7,46 +7,26 @@ import xgboost as xgb
 import pickle as pkl
 from glob import glob
 
-def main():
-    parser = argparse.ArgumentParser()
-    # SageMaker 특정 인자 설정 (기본값은 환경 변수에서 가져옴)
-    parser.add_argument('--output_data_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
-    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
-    parser.add_argument('--validation', type=str, default=os.environ.get('SM_CHANNEL_VALIDATION'))
-    parser.add_argument('--model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
-    # 하이퍼파라미터 설정
-    parser.add_argument('--max-depth', type=int, default=3)
-    parser.add_argument('--learning-rate', type=float, default=0.1)
-    parser.add_argument('--reg-alpha', type=float, default=0)
-    parser.add_argument('--reg-lambda', type=float, default=1)
-    parser.add_argument('--subsample', type=float, default=1)
-    parser.add_argument('--colsample-bytree', type=float, default=1)
-    parser.add_argument('--num-round', type=int, default=200)
-    parser.add_argument('--early-stopping-rounds', type=int, default=10)
-    parser.add_argument('--objective', type=str, default='binary:logistic')
-    parser.add_argument('--eval-metric', type=str, default='auc')
-    args, _ = parser.parse_known_args()
-
-    # 데이터 로드
-    
-    # CSV 파일 목록 가져오기
-    train_files = glob(args.train + "/*.csv")
+def main(args):  
+    # 훈련 / 검증 데이터 로드
+    train_files = glob(os.path.join(args.train, "*.csv"))
+    print(f"훈련 데이터 파일 목록: {train_files}")
     train_data = pd.concat([pd.read_csv(file) for file in train_files], ignore_index=True)
-    val_files = glob(args.validation + "/*.csv")
+    val_files = glob(os.path.join(args.validation, "*.csv"))
+    print(f"검증 데이터 파일 목록: {val_files}")
     val_data = pd.concat([pd.read_csv(file) for file in val_files], ignore_index=True)
 
-    # 특성과 타겟 분리
+    # XGBoost 입력 데이터 형식 변환
     X_train = train_data.iloc[:, 1:]
-    y_train = train_data.iloc[:, 0]
+    y_train = train_data.iloc[:, 0].astype(int)  # 타겟 변수를 정수형으로 변환
     X_val = val_data.iloc[:, 1:]
-    y_val = val_data.iloc[:, 0]
+    y_val = val_data.iloc[:, 0].astype(int)  # 타겟 변수를 정수형으로 변환
     
     d_train = xgb.DMatrix(X_train, label=y_train)
     d_val = xgb.DMatrix(X_val, label=y_val)
 
     # XGBoost 모델 생성 및 훈련
     watchlist = [(d_train, '훈련'), (d_val, '검증')]
-    
     params = {
         'max_depth': args.max_depth,
         'learning_rate': args.learning_rate,
@@ -76,6 +56,27 @@ def main():
     # 모델 저장
     model_path = os.path.join(args.model_dir, 'xgboost-model')
     pkl.dump(xgb_model, open(model_path, 'wb'))
+    print(f'모델이 {model_path}에 저장되었습니다.')
 
 if __name__ == '__main__':
-    main()
+    
+    parser = argparse.ArgumentParser()
+    # SageMaker 특정 인자 설정 (기본값은 환경 변수에서 가져옴)
+    parser.add_argument('--output_data_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    parser.add_argument('--validation', type=str, default=os.environ.get('SM_CHANNEL_VALIDATION'))
+    parser.add_argument('--model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    # 하이퍼파라미터 설정
+    parser.add_argument('--max-depth', type=int, default=3)
+    parser.add_argument('--learning-rate', type=float, default=0.1)
+    parser.add_argument('--reg-alpha', type=float, default=0)
+    parser.add_argument('--reg-lambda', type=float, default=1)
+    parser.add_argument('--subsample', type=float, default=1)
+    parser.add_argument('--colsample-bytree', type=float, default=1)
+    parser.add_argument('--num-round', type=int, default=200)
+    parser.add_argument('--early-stopping-rounds', type=int, default=10)
+    parser.add_argument('--objective', type=str, default='binary:logistic')
+    parser.add_argument('--eval-metric', type=str, default='auc')
+    args, _ = parser.parse_known_args()
+    
+    main(args)
